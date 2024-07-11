f1ed1f0a9708e4936d5744b3eef7501e7ad3bdec
Ayberk Atalay <105879357+AyberkAtalay0@users.noreply.github.com>
2024-07-11 22:37:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.2.1'
+VERSION = '0.2.2'
 DESCRIPTION = 'A pinescript-like financial data analysis and trading package'
 
 from pathlib import Path
